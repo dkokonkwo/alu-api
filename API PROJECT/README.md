486228e92486430cb379cfdb26a4f6e5a1b45d18
dkokonkwo <d.okonkwo@alustudent.com>
2023-08-17 23:14:04 +0200

    api project

---
+++ API PROJECT/README.md
@@ -0,0 +1,48 @@
+# Trivia Question and Answer Python Script
+
+This Python script allows you to fetch trivia questions and answers from a trivia API and display them in your console.
+
+## Features
+
+- Fetch geography trivia questions from the API.
+- Display the questions along with boolean options.
+- Check if the user's selected answer is correct and provide feedback.
+
+## Requirements
+
+- Python 3.x
+- Requests library (you can install it using `pip install requests`)
+
+## Usage
+
+1. Clone this repository or download the `trivia.py` file.
+
+2. Open a terminal or command prompt.
+
+3. Navigate to the directory containing the `trivia.py` file.
+
+4. Run the script using the following command:
+    `python run trivia.py`
+
+5. The script will fetch a trivia question from the API, display the question, and prompt you to select an answer.
+
+6. After selecting an answer, the script will display whether your answer is correct or not.
+
+7. The script will then fetch and display the next question, and the process continues.
+
+
+## Configuration
+
+You can customize the behavior of the script by modifying the following variables in the `trivia.py` file:
+
+- `api_endpooint`: The URL of the trivia API endpoint.
+- `difficulty`: The difficulty of the questions.
+
+## API Used
+
+This script uses the Open Trivia DB https://opentdb.com/api_config.php to fetch trivia questions and answers. Make sure you have a stable internet connection to use this script effectively.
+
+## Disclaimer
+
+This script is provided as-is and is intended for educational purposes only. The accuracy and availability of trivia questions and answers are dependent on the trivia API used.
+
---
+++ API PROJECT/trivia.py
@@ -0,0 +1,62 @@
+import json
+import requests
+import time
+
+
+print("WELCOME TO TRIVIA ARENA! \nTest your knowledge in Geography\n")
+time.sleep(1)
+print("Stand a chance to win a columbus medal if you can answer all correctly\n")
+time.sleep(3)
+print("You have three tries to to answer ten questions. How far can you go?\n")
+time.sleep(2)
+
+difficulty = ''
+
+while difficulty not in ['easy', 'medium']:
+    difficulty = input("Please choose your difficulty: Easy(e) or Medium(m)?: ")
+    if difficulty == 'e':
+        difficulty = 'easy'
+    elif difficulty == 'm':
+        difficulty = 'medium'
+    else:
+        print("please input 'e', 'm' or 'h'")
+
+api_endpoint = f"https://opentdb.com/api.php?amount=10&category=22&difficulty={difficulty}&type=boolean"
+api_info = requests.get(
+    api_endpoint,
+    headers={'user-agent': 'user'},
+    allow_redirects=False
+).json()
+
+results = api_info["results"]
+
+numberOfTries = 0
+score = 0
+
+for i in range(len(results)):
+    if numberOfTries == 3:
+        print("Oops! Seems like you are out of tries. Try again?")
+        print(score)
+        break
+    else:
+        time.sleep(1)
+        print(results[i]['question'])
+        print()
+        answer = ''
+        while answer not in ['True', 'False']:
+            answer = input("True(t) or False(f)? ")
+            if answer == "t":
+                answer = "True"
+            elif answer == "f":
+                answer = "False"
+            else:
+                print("Please give a valid input\n")
+        if answer == results[i]['correct_answer']:
+            score += 1
+            print("correct +1!\n")
+        else:
+            numberOfTries += 1
+            print("Not correct!\n")
+if score == 10:
+    print('legend! You have won!!\n')
+print(f"You scored {score} points!")
